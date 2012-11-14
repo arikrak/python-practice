97a1d18cf8e7ff507f894a7c2e2e0aca2661fce3
Ariel Krakowksi <arikrak@gmail.com>
2012-11-13 20:26:35 -0500

    initial python file

---
+++ hello.py
@@ -0,0 +1,28 @@
+#code to print all permutations of a string
+#in current form to solve Project Euler problem #24 (though it can be solved by hand also)
+
+num=0
+
+def permute(prev_str, str):
+	global num
+	if(len(str) == 0 ):
+		num +=1
+		if(num==1000000):
+			print(prev_str)
+	else:
+		for i in range(len(str)):
+			permute( prev_str+str[i], str[:i] + str[i+1:])	
+
+permute("", "0123456789")
+#permute("", "abcde")
+
+
+
+
+
+
+
+
+
+
+
